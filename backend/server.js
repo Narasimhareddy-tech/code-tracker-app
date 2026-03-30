c14d5e6e0f542fdd8ceb7056730d17a3355abf1c
Narasimhareddy-tech <lnarasimhareddy77@gmail.com>
2026-03-30 22:10:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 require("dotenv").config();
-
 const connectDB = require("./config/db");
 
 const app = express();
 
-// 🔥 MIDDLEWARE
-app.use(cors({
-  origin: "*"
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-// 🔥 CONNECT DB
 connectDB();
 
-// 🔥 ROUTES
 const leetcodeRoute = require("./routes/leetcode");
 app.use("/api/leetcode", leetcodeRoute);
 
@@ -41,22 +35,12 @@ app.use("/api/user", userRoute);
 const cfRoute = require("./routes/codeforces");
 app.use("/api/codeforces", cfRoute);
 
-//  TEST ROUTE
 app.get("/", (req, res) => {
   res.send("CodeTracker API running");
 });
 
-//  SERVE FRONTEND (React build)
-//app.use(express.static(path.join(__dirname, "frontend", "build")));
-
-//app.get('(.*)', (req, res) => {
- // res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
-//});
-
-  //PORT (IMPORTANT FOR DEPLOYMENT)
 const PORT = process.env.PORT || 3000;
 
-//  START SERVER
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`);
-//});
+app.listen(PORT, function() {
+  console.log("Server running on port " + PORT);
+});
