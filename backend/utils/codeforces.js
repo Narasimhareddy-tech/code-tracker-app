e718a8918650896b3a250f5f0b457bd8e6ee9dfb
Narasimhareddy-tech <lnarasimhareddy77@gmail.com>
2026-04-14 22:33:33 +0530

    Update codeforces.js

--- backend/utils/codeforces.js
+++ backend/utils/codeforces.js
@@ -1,35 +1,5 @@
-const axios = require("axios");
-
 async function getCodeforcesStats(username) {
-    console.log("🔥 CF FUNCTION CALLED"); 
-  try {
-    const res = await axios.get(
-      `https://codeforces.com/api/user.status?handle=${username}`
-    );
-
-    if (res.data.status !== "OK") {
-      return { totalSolved: 0 };
-    }
-
-    const solvedSet = new Set();
-
-    for (const sub of res.data.result) {
-      if (sub.verdict === "OK") {
-        const id = sub.problem.contestId + "-" + sub.problem.index;
-        solvedSet.add(id);
-      }
-    }
-
-    console.log("CF SOLVED COUNT:", solvedSet.size); // 🔥 DEBUG
-
-    return {
-      totalSolved: solvedSet.size,
-    };
-
-  } catch (err) {
-    console.log("CF ERROR:", err.message);
-    return { totalSolved: 0 };
-  }
+  return { totalSolved: 999 }; // 🔥 FORCE VALUE
 }
 
 module.exports = getCodeforcesStats;
