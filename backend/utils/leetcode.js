51ecd42b21c46b06bd5e9e5efaf9647bb8a847dc
Narasimhareddy-tech <lnarasimhareddy77@gmail.com>
2026-04-14 22:06:44 +0530

    Update leetcode.js

--- backend/utils/leetcode.js
+++ backend/utils/leetcode.js
@@ -2,15 +2,34 @@ const axios = require("axios");
 
 async function getLeetCodeStats(username) {
   try {
-    const res = await axios.get(
-      `https://leetcode-stats-api.herokuapp.com/${username}`
-    );
+    const res = await axios.post("https://leetcode.com/graphql", {
+      query: `
+        query getUserProfile($username: String!) {
+          matchedUser(username: $username) {
+            submitStats: submitStatsGlobal {
+              acSubmissionNum {
+                difficulty
+                count
+              }
+            }
+          }
+        }
+      `,
+      variables: { username }
+    });
 
-    return {
-      easy: res.data.easySolved || 0,
-      medium: res.data.mediumSolved || 0,
-      hard: res.data.hardSolved || 0,
-    };
+    const stats = res.data.data.matchedUser.submitStats.acSubmissionNum;
+
+    let easy = 0, medium = 0, hard = 0;
+
+    stats.forEach(s => {
+      if (s.difficulty === "Easy") easy = s.count;
+      if (s.difficulty === "Medium") medium = s.count;
+      if (s.difficulty === "Hard") hard = s.count;
+    });
+
+    return { easy, medium, hard };
+
   } catch (err) {
     console.log("LC ERROR:", err.message);
     return { easy: 0, medium: 0, hard: 0 };
