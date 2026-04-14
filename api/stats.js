d647457130860804e9f04e35e950617017c3019b
Narasimhareddy-tech <lnarasimhareddy77@gmail.com>
2026-04-14 22:40:14 +0530

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -1,1 +1,20 @@
 
+export default async function handler(req, res) {
+  const { lc, cc, cf } = req.query;
+
+  return res.status(200).json({
+    leetcode: { easy: 10, medium: 20, hard: 5 },
+    codechef: { totalSolved: 50, rating: 1500 },
+    codeforces: { totalSolved: 999 },
+    combined: {
+      easy: 10,
+      medium: 20,
+      hard: 5,
+      totalSolved: 35,
+      rating: 1500,
+    },
+    leetcodeUsername: lc,
+    codechefUsername: cc,
+    codeforcesUsername: cf,
+  });
+}
