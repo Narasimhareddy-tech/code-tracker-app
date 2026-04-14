e5fa4a8f693e56e76455ac24755900dea70fd2a4
Narasimhareddy-tech <lnarasimhareddy77@gmail.com>
2026-04-14 21:49:50 +0530

    Update leetcode.js

--- backend/utils/leetcode.js
+++ backend/utils/leetcode.js
@@ -1,1 +1,20 @@
+const axios = require("axios");
 
+async function getLeetCodeStats(username) {
+  try {
+    const res = await axios.get(
+      `https://leetcode-stats-api.herokuapp.com/${username}`
+    );
+
+    return {
+      easy: res.data.easySolved || 0,
+      medium: res.data.mediumSolved || 0,
+      hard: res.data.hardSolved || 0,
+    };
+  } catch (err) {
+    console.log("LC ERROR:", err.message);
+    return { easy: 0, medium: 0, hard: 0 };
+  }
+}
+
+module.exports = getLeetCodeStats;
