657bf30fe0d46a77168fccb21bd2b6e1d99a7699
Narasimhareddy-tech <lnarasimhareddy77@gmail.com>
2026-05-01 09:30:14 +0530

    Delete backend/utils directory

--- backend/utils/codechef.js
+++
@@ -1,8 +0,0 @@
-async function getCodechefStats(username) {
-  return {
-    totalSolved: 0,
-    rating: 0,
-  };
-}
-
-module.exports = getCodechefStats;
--- backend/utils/codeforces.js
+++
@@ -1,5 +0,0 @@
-async function getCodeforcesStats(username) {
-  return { totalSolved: 999 }; // 🔥 FORCE VALUE
-}
-
-module.exports = getCodeforcesStats;
